9e3ad515c40192384917b3b7fbe39de70c11e2f9
taroj khan <tarujkhan@yahoo.com>
2020-04-12 19:49:12 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,12 +3,12 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.genres.first
+    self.genres.first.genre
     #return the genre of the artist's first saved song
   end
 
   def song_count
-    genres.count(:songs)
+    self.genres.count
     #return the number of songs associated with the artist
   end
 
